18ade0628dd1bda06c9abce4c3ae129dee119d36
zhangmingbo <252164303@qq.com>
2019-09-19 15:55:32 +0800

    update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import TodoList from './TodoList'
+
+class App extends Component{
+  render() {
+    return(
+      <TodoList></TodoList>
+    )
+  };
+}
+
+export default App;
---
+++ src/store/reducer.js
@@ -0,0 +1,8 @@
+const defauleState = {
+  inputValue: '123',
+  list: [1, 2]
+}
+
+export default (state = defauleState, actions) =>{
+  return state
+}
